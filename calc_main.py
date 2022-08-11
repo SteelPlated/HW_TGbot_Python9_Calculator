ec115e23ac8ca5a661958612d06020dcb4a4e663
SteelPlated <103888886+SteelPlated@users.noreply.github.com>
2022-08-11 20:13:12 +0500

    Update calc_main.py

--- calc_main.py
+++ calc_main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler
 from calc import*
 
-updater = Updater(token='5572182114:AAEGaREq4XiSrXJdUjxRNteu2Bd0pBD4VUM')
+updater = Updater(token='')
 updater.dispatcher.add_handler(CommandHandler('sum', summa_command))
 updater.dispatcher.add_handler(CommandHandler('sub', subtraction_command))
 updater.dispatcher.add_handler(CommandHandler('seg', segmentation_command))
